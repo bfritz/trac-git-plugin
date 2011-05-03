99d636f42cdbcbedc25501bb159c6868b6e6d401
Don Marti <dmarti@zgp.org>
2011-05-03 09:58:41 -0700

    Update setup.py with fork info, bump version  On branch master

--- setup.py
+++ setup.py
@@ -9,12 +9,17 @@ setup(name='TracGit',
       author_email='hvr@gnu.org',
       keywords='trac scm plugin git',
       url="http://trac-hacks.org/wiki/GitPlugin",
-      version='0.12.0.5',
+      version='0.12.0.6',
       license="GPL",
       long_description="""
       This Trac 0.12 plugin provides support for the GIT SCM.
 
       See http://trac-hacks.org/wiki/GitPlugin for more details.
+
+      This is a forked version from 
+        https://github.com/dmarti/trac-git-plugin
+      This version does more mapping of old Subversion data, for
+      converted repositories.
       """,
       packages=['tracext', 'tracext.git'],
       namespace_packages=['tracext'],
